c45eae5ab63d2b2e15ff22c4def0b41f7c43a7b8
Sujith S <116474043+su-jin1425@users.noreply.github.com>
2024-08-11 21:46:21 +0530

    -

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return <div className="home">
     <div className="logo">Velorea<sup>©</sup></div>
     <div className="credit">
-        <a href="https://su-jin1425.github.io/Shoot_The_Breeze/" className="insta">Shoot the Breeze - Studio</a>
+        <a href="https://shoot-the-breeze.vercel.app/" className="insta">Shoot the Breeze - Studio</a>
     </div>
     <div className="header">
         <div className="title">
             <span className="slant">Eternal</span> elegance <br />
             season by season
         </div>
-        <a href="https://su-jin1425.github.io/Shoot_The_Breeze/" className="myButton">Shop now</a>
+        <a href="https://shoot-the-breeze.vercel.app/" className="myButton">Shop now</a>
         <img src={HEAD} alt="" className="head" />
     </div>
   </div>;
